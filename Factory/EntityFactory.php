5cfc5d686eb4b42185eff57bb3e574306ec21d26
Christopher West <9ninemind@gmail.com>
2015-04-22 15:12:15 -0600

    adds entity factory

---
+++ Factory/EntityFactory.php
@@ -0,0 +1,30 @@
+<?php
+namespace BlackBoxCode\Pando\Bundle\BaseBundle\Factory;
+
+class EntityFactory
+{
+    /**
+     * @var string
+     */
+    private $entityNamespace;
+
+    /**
+     * @param $entityNamespace
+     */
+    public function __construct($entityNamespace)
+    {
+        $this->entityNamespace = $entityNamespace;
+    }
+
+    /**
+     * Returns a new instance of the Entity found with the given name
+     *
+     * @param string $entityName
+     * @return mixed
+     */
+    public function create($entityName)
+    {
+        $fullNamespace = '\\' . $this->entityNamespace . '\\' . $entityName;
+        return new $fullNamespace();
+    }
+}
