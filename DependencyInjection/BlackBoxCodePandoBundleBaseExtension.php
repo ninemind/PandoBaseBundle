dea8c578d2959a840ffc84bec73068097c62b5a0
Christopher West <9ninemind@gmail.com>
2015-04-22 15:15:01 -0600

    loads factory classes

--- DependencyInjection/BlackBoxCodePandoBundleBaseExtension.php
+++ DependencyInjection/BlackBoxCodePandoBundleBaseExtension.php
@@ -23,6 +23,7 @@ class BlackBoxCodePandoBundleBaseExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('factories.yml');
         $loader->load('services.yml');
     }
 }
