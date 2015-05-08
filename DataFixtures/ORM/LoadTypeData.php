72d5cf438264b46c6292caf2893ecaf16ecb1b58
Christopher West <9ninemind@gmail.com>
2015-05-08 11:34:04 -0600

    adds fixture stub

---
+++ DataFixtures/ORM/LoadTypeData.php
@@ -0,0 +1,16 @@
+<?php
+namespace BlackBoxCode\Pando\Bundle\BaseBundle\DataFixtures;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadTypeData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
