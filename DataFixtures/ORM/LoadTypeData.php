833d9f19e53e1d5a836a1d8e05c0aa4543c9d2c5
Christopher West <9ninemind@gmail.com>
2015-05-08 12:23:59 -0600

    adds fixture to populate type tables

--- DataFixtures/ORM/LoadTypeData.php
+++ DataFixtures/ORM/LoadTypeData.php
@@ -3,14 +3,44 @@ namespace BlackBoxCode\Pando\Bundle\BaseBundle\DataFixtures;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\ClassLoader\ClassMapGenerator;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadTypeData implements FixtureInterface
+class LoadTypeData implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+
     /**
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
     {
+        $classMap = ClassMapGenerator::createMap('src' . DIRECTORY_SEPARATOR . $this->container->getParameter('pando_entity.namespace'));
 
+        foreach ($classMap as $ns => $file) {
+            if (substr($ns, -4) === 'Type') {
+                $r = new \ReflectionClass($ns);
+                foreach ($r->getConstants() as $constant) {
+                    $type = new $ns;
+                    $type->setName($constant);
+                    $manager->persist($type);
+                }
+            }
+        }
+
+        $manager->flush();
     }
 }
